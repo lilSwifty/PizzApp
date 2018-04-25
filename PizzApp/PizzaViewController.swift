63b3fb9e16cd6d8ea82f86e86131a507f7de50d3
Mani Sedighi <manisedighi@Apples-MacBook-Pro.local>
2018-04-25 21:52:09 +0200

    fixed tableview problem

--- PizzApp/PizzaViewController.swift
+++ PizzApp/PizzaViewController.swift
@@ -128,9 +128,14 @@ extension PizzaViewController: UITableViewDelegate, UITableViewDataSource{
             
             if let destinationVC = segue.destination as? PizzaPreviewViewController{
 
-                    destinationVC.pizzaToOrder = pizzaList[(tableview.indexPathForSelectedRow?.row)!]
+                switch tableview.indexPathForSelectedRow?.section {
+                case 0: destinationVC.pizzaToOrder = listOfAllPizzas[(tableview.indexPathForSelectedRow?.row)!]
+                case 1: destinationVC.pizzaToOrder = listOfAllPanPizzas[(tableview.indexPathForSelectedRow?.row)!]
+                case 2: destinationVC.pizzaToOrder = listOfAllSpecialPizzas[(tableview.indexPathForSelectedRow?.row)!]
+                default : break
+                }
 
-            }            
+            }
             
         }
     
@@ -158,180 +163,143 @@ extension PizzaViewController: UITableViewDelegate, UITableViewDataSource{
 extension PizzaViewController {
     func updatePizzaInformation(){
         
-        var listOfAllToppings : [String : Double] = ["Champnjoner" : 1.1,
-                                                     "Ananas" : 1.1,
-                                                     "Paprika" : 1.1,
-                                                     "Lök" : 1.1,
-                                                     "Syltlök" : 1.1,
-                                                     "Oliver" : 1.1,
-                                                     "Majs" : 1.1,
-                                                     "Saltgurka" : 1.1,
-                                                     "Jalapeno" : 1.1,
-                                                     "Chilisås" : 1.1,
-                                                     "Vitlök" : 1.1,
-                                                     "Skinka" : 1.3,
-                                                     "Bacon" : 1.3,
-                                                     "Salami" : 1.3,
-                                                     "Maletköttsås" : 1.3,
-                                                     "Ägg" : 1.3,
-                                                     "Anjovis" : 1.3,
-                                                     "Ost" : 1.3,
-                                                     "Fetaost" : 1.3,
-                                                     "Auraost" : 1.3,
-                                                     "Färsk tomat" : 1.3,
-                                                     "Banan" : 1.3,
-                                                     "Kebabkött" : 1.6,
-                                                     "Bearneisesås" : 1.6,
-                                                     "Soltorkade tomater" : 1.6,
-                                                     "Tonfisk" : 1.6,
-                                                     "Räkor" : 1.6,
-                                                     "Musslor" : 1.6,
-                                                     "Oxinrefilé" : 4.1]
+        var listOfAllToppings : [String : Double] = ["1 Champnjoner" : 1.1,
+                                                     "2 Ananas" : 1.1,
+                                                     "3 Paprika" : 1.1,
+                                                     "4 Lök" : 1.1,
+                                                     "5 Syltlök" : 1.1,
+                                                     "6 Oliver" : 1.1,
+                                                     "7 Majs" : 1.1,
+                                                     "8 Saltgurka" : 1.1,
+                                                     "9 Jalapeno" : 1.1,
+                                                     "10 Chilisås" : 1.1,
+                                                     "11 Vitlök" : 1.1,
+                                                     "12 Skinka" : 1.3,
+                                                     "13 Bacon" : 1.3,
+                                                     "14 Salami" : 1.3,
+                                                     "15 Maletköttsås" : 1.3,
+                                                     "16 Ägg" : 1.3,
+                                                     "17 Anjovis" : 1.3,
+                                                     "18 Ost" : 1.3,
+                                                     "19 Fetaost" : 1.3,
+                                                     "20 Auraost" : 1.3,
+                                                     "21 Färsk tomat" : 1.3,
+                                                     "22 Banan" : 1.3,
+                                                     "23 Kebabkött" : 1.6,
+                                                     "24 Bearneisesås" : 1.6,
+                                                     "25 Soltorkade tomater" : 1.6,
+                                                     "26 Tonfisk" : 1.6,
+                                                     "27 Räkor" : 1.6,
+                                                     "28 Musslor" : 1.6,
+                                                     "19 Oxinrefilé" : 4.1]
         
         
         // All regular pizzas :
         let mexicana = Pizza(name: "Mexicana", topping: ["Maletköttsås", "Paprika", "Majs"], price: 11.30)
         listOfAllPizzas.append(mexicana)
-        pizzaList.append(mexicana)
         
         let capricciosa = Pizza(name: "Capricciosa", topping: ["Skinka", "Champinjoner"], price: 11.70)
         listOfAllPizzas.append(capricciosa)
-        pizzaList.append(capricciosa)
         
         let fruttiDiMare = Pizza(name: "Frutti Di Mare", topping: ["Musslor", "Räkor"], price: 10.90)
         listOfAllPizzas.append(fruttiDiMare)
-        pizzaList.append(fruttiDiMare)
         
         let operaSpecial = Pizza(name: "Opera Special", topping: ["Skinka" , "Tonfisk" , "Salami"], price: 11.70)
         listOfAllPizzas.append(operaSpecial)
-        pizzaList.append(operaSpecial)
         
         let osoleMio = Pizza(name: "O'Sole Mio", topping: ["Tonfisk", "Räkor"], price: 10.90)
         listOfAllPizzas.append(osoleMio)
-        pizzaList.append(osoleMio)
         
         let bussola = Pizza(name: "Bussola", topping: ["Skinka", "Räkor"], price: 10.90)
         listOfAllPizzas.append(bussola)
-        pizzaList.append(bussola)
         
         let opera = Pizza(name: "Opera", topping: ["Skinka", "Tonfisk"], price: 10.90)
         listOfAllPizzas.append(opera)
-        pizzaList.append(opera)
         
         let siciliana = Pizza(name: "Siciliana", topping: ["Anjovis", "Oliver", "Vitlök"], price: 11.30)
         listOfAllPizzas.append(siciliana)
-        pizzaList.append(siciliana)
         
         let calzone = Pizza(name: "Calzone", topping: ["Skinka", "inbakad"], price: 11.90)
         listOfAllPizzas.append(calzone)
-        pizzaList.append(calzone)
         
         let vegetariana = Pizza(name: "Vegetariana", topping: ["Champinjoner", "Lök", "Paprika", "Oliver", "Ananas"], price: 12.30)
         listOfAllPizzas.append(vegetariana)
-        pizzaList.append(vegetariana)
         
         let americana = Pizza(name: "Americana", topping: ["Skinka", "Ananas", "Auraost"], price: 11.50)
         listOfAllPizzas.append(americana)
-        pizzaList.append(americana)
         
         let vesuvio = Pizza(name: "Vesuvio", topping: ["Skinka"], price: 9.50)
         listOfAllPizzas.append(vesuvio)
-        pizzaList.append(vesuvio)
         
         let quattro = Pizza(name: "Quattro Stagioni", topping: ["Skinka", "Champinjoner", "Räkor", "Musslor" ], price: 12.10)
         listOfAllPizzas.append(quattro)
-        pizzaList.append(quattro)
         
         let cacciatora = Pizza(name: "Cacciatora", topping: ["Salami", "Oliver"], price: 10.70)
         listOfAllPizzas.append(cacciatora)
-        pizzaList.append(cacciatora)
         
         // All pan pizzas :
         
         let peterpan = Pizza(name: "Peter Pan", topping: ["Skinka", "Champinjoner", "Ananas"], price: 10.90)
         listOfAllPanPizzas.append(peterpan)
-        pizzaList.append(peterpan)
         
         let streetman = Pizza(name: "Streetman", topping: ["Maletköttsås", "Salami", "Lök","Paprika"], price: 11.70)
         listOfAllPanPizzas.append(streetman)
-        pizzaList.append(streetman)
         
         let texaspan = Pizza(name: "Texas Pan", topping: ["Bacon", "Syltlök", "Auraost"], price: 11.10)
         listOfAllPanPizzas.append(texaspan)
-        pizzaList.append(texaspan)
         
         let fruitland = Pizza(name: "Fruitland", topping: ["Champinjoner", "Ananas","Lök", "Oliver"], price: 11.30)
         listOfAllPanPizzas.append(fruitland)
-        pizzaList.append(fruitland)
         
         let trinity = Pizza(name: "Trinity River", topping: ["Tonfisk", "Räkor", "Lök"], price: 11.10)
         listOfAllPanPizzas.append(trinity)
-        pizzaList.append(trinity)
         
         let regency = Pizza(name: "Regency", topping: ["Skinka", "Ananas", "Auraost"], price: 11.10)
         listOfAllPanPizzas.append(regency)
-        pizzaList.append(regency)
         
         let dalhart = Pizza(name: "Dalhart", topping: ["Salami", "Lök", "Paprika", "Saltgurka", "Auraost"], price: 12.30)
         listOfAllPanPizzas.append(dalhart)
-        pizzaList.append(dalhart)
         
         // All special pizzas :
         
         let sophia = Pizza(name: "Sophia Loren", topping: ["Bacon", "Ananas", "Ägg"], price: 11.50)
         listOfAllSpecialPizzas.append(sophia)
-        pizzaList.append(sophia)
         
         let pompeij = Pizza(name: "Pompeij", topping: ["Bacon", "Lök"], price: 10.70)
         listOfAllSpecialPizzas.append(pompeij)
-        pizzaList.append(pompeij)
         
         let pekkas = Pizza(name: "Pekkas Special", topping: ["Skinka","Champinjoner", "Räkor", "Ananas", "Auraost" ], price: 12.90)
         listOfAllSpecialPizzas.append(pekkas)
-        pizzaList.append(pekkas)
         
         let calzoneD = Pizza(name: "Calzone a'la Dallas", topping: ["Skinka", "Lök", "Champinjoner", "Auraost", "inbakad"], price: 13.50)
         listOfAllSpecialPizzas.append(calzoneD)
-        pizzaList.append(calzoneD)
         
         let vegeDallas = Pizza(name: "Vegetariana a'la Dallas", topping: ["Champinjoner", "Lök", "Paprika", "Oliver", "Ananas", "Auraost"], price: 13.10)
         listOfAllSpecialPizzas.append(vegeDallas)
-        pizzaList.append(vegeDallas)
         
         let balaton = Pizza(name: "Balaton", topping: ["Salami", "Saltgurka", "Lök", "Auraost"], price: 12.10)
         listOfAllSpecialPizzas.append(balaton)
-        pizzaList.append(balaton)
         
         let westerwik = Pizza(name: "Pizza Westerwik", topping: ["Maletköttsås", "Saltgurka", "Champinjoner", "Lök"], price: 11.90)
         listOfAllSpecialPizzas.append(westerwik)
-        pizzaList.append(westerwik)
         
         let diablo = Pizza(name: "Diablo (stark)", topping: ["Chilisås", "Pepperonisalami", "Ananas", "Lök", "Jalapeno"], price: 12.50)
         listOfAllSpecialPizzas.append(diablo)
-        pizzaList.append(diablo)
         
         let dallas = Pizza(name: "Dallas", topping: ["Oxinnerfilé 2 x 50 g", "Majs", "Lök", "Ägg"], price: 18.50)
         listOfAllSpecialPizzas.append(dallas)
-        pizzaList.append(dallas)
         
         let dallasSpecial = Pizza(name: "Dallas Special", topping: ["Oxinnerfilé 2 x 50 g", "Majs", "Lök", "Auraost"], price: 19.10)
         listOfAllSpecialPizzas.append(dallasSpecial)
-        pizzaList.append(dallasSpecial)
         
         let bobafett = Pizza(name: "Boba Fett", topping: ["Strimlad oxinnerfilé 50g", "Fetaost", "Skivad tomat"], price: 15.70)
         listOfAllSpecialPizzas.append(bobafett)
-        pizzaList.append(bobafett)
         
         let banana = Pizza(name: "Banana", topping: ["Skinka", "Banan", "Curry"], price: 11.50)
         listOfAllSpecialPizzas.append(banana)
-        pizzaList.append(banana)
         
         let sweden = Pizza(name: "Dr. Sweden", topping: ["Kebab", "Soltorkade tomater", "Bearneisesås"], price: 12.60)
         listOfAllSpecialPizzas.append(sweden)
-        pizzaList.append(sweden)
-        
-        
-        
-        
+
     }
 }
